b3950d95dc5633aced5549514c1f7c225ccec286
Slackoth <00082318@uca.edu.sv>
2021-05-05 08:13:56 -0500

    styles module completed

--- 2021/react-guide-2/src/components/UI/Button/Button.js
+++ 2021/react-guide-2/src/components/UI/Button/Button.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Style from './Button.module.css';
+/*import Styled from 'styled-components';
 
-import './Button.css';
+const Button = Styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+  @media (min-width: 768px) {
+    width: auto
+  }
+ `;*/
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={Style.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
